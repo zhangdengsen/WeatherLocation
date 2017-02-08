13108c61494f03c17e9425f0b86874a77a5d1585
Weather <https://github.com/zhangdengsen>
2017-02-09 07:28:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/v5188/weatherlocation/model/Province.java
@@ -0,0 +1,35 @@
+package com.example.v5188.weatherlocation.model;
+
+/**
+ * Created by v5188 on 2017/2/7.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
